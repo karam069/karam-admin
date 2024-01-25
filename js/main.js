70d30ea254e54b68ab9dee36920edc2c77a1e74e
Karam <storiesmorekaram@gmail.com>
2024-01-25 18:38:39 +0400

    uploaded the new files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 let input = document.getElementById("search-input");
+let post = document.getElementById("post-txt");
+let postComment = document.getElementById("post-comment");
+
 
 input.onclick = function(){
     input.style.paddingRight = "30px";
+}
+
+post.onclick = function(){
+    post.style.color = "red";
+}
+postComment.onclick = function(){
+    postComment.style.color = "red";
 }
